6621df070d196847ec1e306c95e8b3b5bc5ae1e7
nandeeta <pantn580@strose.edu>
2019-01-27 12:32:48 -0500

    Added Java project

---
+++ SoftEngLab1/src/Hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Hello {
+	public static void main (String[] argv)
+	{
+		
+		Scanner Keyboard = new Scanner (System.in);
+		System.out.print("Enter the user name: ");
+		String s;
+		s = Keyboard.next();
+		System.out.println(s);
+		
+		
+	}
+	}
+
