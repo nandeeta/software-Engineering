31515973e8ed9b20e95a109544ea169d64051bbb
nandeeta <pantn580@strose.edu>
2019-01-27 12:38:17 -0500

    Removed Extra lines

--- SoftEngLab1/src/Hello.java
+++ SoftEngLab1/src/Hello.java
@@ -1,15 +1,12 @@
 import java.util.Scanner;
 public class Hello {
 	public static void main (String[] argv)
-	{
-		
+	{		
 		Scanner Keyboard = new Scanner (System.in);
 		System.out.print("Enter the user name: ");
 		String s;
 		s = Keyboard.next();
-		System.out.println(s);
-		
-		
+		System.out.println(s);			
 	}
 	}
 
